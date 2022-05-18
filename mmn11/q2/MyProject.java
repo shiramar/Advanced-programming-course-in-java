9622b430feb9b865ab470aee2159705b18ff3584
shiramar <86928995+shiramar@users.noreply.github.com>
2022-05-18 09:44:11 +0300

    Update MyProject.java

--- mmn11/q2/MyProject.java
+++ mmn11/q2/MyProject.java
@@ -1,1 +1,22 @@
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyProject extends Application{
+
+    public void start(Stage stage) throws Exception{
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("MyProject.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("MyProject");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println();
+    }
+}
