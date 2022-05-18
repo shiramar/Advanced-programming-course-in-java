505b2925b1ccadb2530e3b9d36dbbf84fb61a32c
shiramar <86928995+shiramar@users.noreply.github.com>
2022-05-18 09:49:05 +0300

    Update Mamal.java

--- mmn12/q1/Mamal.java
+++ mmn12/q1/Mamal.java
@@ -1,1 +1,27 @@
 
+public abstract class Mamal extends Animal{
+
+    //constructor
+    public Mamal(String name, int age, Color color) {
+        super(name, age, color);
+    }
+
+    public abstract void eat();
+
+    public void sleep(){
+        System.out.println("Good night, the mammalian animal is going to sleep.\n");
+    }
+
+    public String toString(){
+        return("mammalian animal: name -"+super.getName()+", age -"+super.getAge()+" color- "+super.getColor());
+    }
+
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public Object clone() {
+        return super.clone();
+    }
+
+}
